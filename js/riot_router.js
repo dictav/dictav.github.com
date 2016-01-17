2060b666326620c57e4d859cb7e2fd9c37811220
Shintaro Abe <dictav@gmail.com>
2016-01-17 20:17:06 +0900

    Fix lint

--- js/riot_router.js
+++ js/riot_router.js
@@ -1,36 +1,38 @@
-window.openWithRiot = function(tag, opts) {
-  console.log('OPEN', tag);
-  riot.compile("tags/" + tag + ".html", function() {
-    riot.mount("div#app", tag, opts);
+/* global riot */
+
+function openWithRiot (tag, opts) {
+  console.log('OPEN', tag)
+  riot.compile('tags/' + tag + '.html', function () {
+    riot.mount('div#app', tag, opts)
 
     // Tracking
-    window.optimizely = window.optimizely || [];
-    window.optimizely.push(["activate", 4443712297]);
-    window.optimizely.push("activateUniversalAnalytics");
-    window.ga('send', 'pageview');
-  });
-};
+    window.optimizely = window.optimizely || []
+    window.optimizely.push(['activate', 4443712297])
+    window.optimizely.push('activateUniversalAnalytics')
+    window.ga('send', 'pageview')
+  })
+}
 
-/// Routes
-riot.compile("tags/ds-nav-layout.html", function(tag){
-  window.navigationTag = riot.mount("ds-nav-layout");
-});
-
-riot.route('/', function(name) {
-  openWithRiot("ds-top");
+// Routes
+riot.compile('tags/ds-nav-layout.html', function (tag) {
+  window.navigationTag = riot.mount('ds-nav-layout')
 })
 
-riot.route('/app/*', function(name) {
-  openWithRiot("ds-app", {name: name});
+riot.route('/', function (name) {
+  openWithRiot('ds-top')
 })
 
-riot.route('/blog', function(name) {
-  openWithRiot("ds-blog", {name: name});
+riot.route('/app/*', function (name) {
+  openWithRiot('ds-app', {name: name})
 })
 
-riot.route('/slides', function(name) {
-  openWithRiot("ds-app", {name: name});
+riot.route('/blog', function (name) {
+  openWithRiot('ds-blog', {name: name})
+})
+
+riot.route('/slides', function (name) {
+  openWithRiot('ds-app', {name: name})
 })
 
 riot.route.base('#!')
-riot.route.start(true);
+riot.route.start(true)
